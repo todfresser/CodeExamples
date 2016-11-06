0a4bdc8adaa0f0d96340b2e5c2960b9c21d7fc45
todfresser <xbox@lange-os.de>
2016-11-06 19:41:24 +0100

    Example added

---
+++ src/todfresser/lobby/main/Example.java
@@ -0,0 +1,16 @@
+package todfresser.lobby.main;
+
+
+public class Example {
+	/***
+	 * Variablen:
+	 * Modifier(static,public,private,abstract,final,protected,...) + ObjectArt(String,int,Rank,...) + Name;
+	 */
+	
+	/***
+	 * Methoden:
+	 * Modifier(static,public,private,final,...) + Rückgabe(void oder eine ObjectArt(String, int, Rank, List<>,...)) + Name der Funktion + (Objecte die übergeben werden sollen: ObjectArt(String, Rank,...) + Name) + ;
+	 */
+	
+
+}
