d6fc43e622a9e4a43121e2206de212072773911b
todfresser <xbox@lange-os.de>
2016-11-06 19:43:21 +0100

    some additions

--- src/todfresser/lobby/main/Example.java
+++ src/todfresser/lobby/main/Example.java
@@ -6,11 +6,15 @@ public class Example {
 	 * Variablen:
 	 * Modifier(static,public,private,abstract,final,protected,...) + ObjectArt(String,int,Rank,...) + Name;
 	 */
+	public int ichbineinInteger;
 	
 	/***
 	 * Methoden:
 	 * Modifier(static,public,private,final,...) + Rückgabe(void oder eine ObjectArt(String, int, Rank, List<>,...)) + Name der Funktion + (Objecte die übergeben werden sollen: ObjectArt(String, Rank,...) + Name) + ;
 	 */
+	public int getMyInteger(String s){
+		return ichbineinInteger;
+	}
 	
 
 }
